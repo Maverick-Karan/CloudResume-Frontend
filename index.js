1facee7353712eb160e66359f8dbc76f6dd70c36
Karan <>
2023-03-12 20:37:48 -0400

    intial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+$(document).ready(() => {
+    $.post('https://54zg0mfw5i.execute-api.us-east-1.amazonaws.com/Dev/apiresource')
+    .done(visitor_counter => {
+        $('#loader').hide();
+        $('#visits').text(visitor_counter);
+
+    })
+    .fail(e => {
+        console.log('Error');
+        console.log(e);
+    });
+});
+
+$(document).ready(function (e) {
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclick($win, $navbar, width);
+
+    // resize event
+    $win.resize(function () {
+        toggle_onclick($win, $navbar, width);
+    });
+
+    $toggle.click(function (e) {
+        $navbar.toggleClass("toggle-left");
+    })
+
+});
+
+function toggle_onclick($win, $navbar, width) {
+    if ($win.width() <= 768) {
+        $navbar.css({ left: `-${width}px` });
+    } else {
+        $navbar.css({ left: '0px' });
+    }
+}
+
+var typed = new Typed('#typed', {
+    strings: [
+        'Cloud Engineer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+var typed_2 = new Typed('#typed_2', {
+    strings: [
+        'Cloud Engineer',
+        'Cloud Architect',
+        'DevOps Engineer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
